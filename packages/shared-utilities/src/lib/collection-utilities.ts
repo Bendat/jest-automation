f5cb9a51c865dcbd20bc851496649c51ccdd716c
Ben Aherne <Bendat@users.noreply.github.com>
2022-08-30 14:48:18 +0100

    docs: added documentation for isEmpty function

--- packages/shared-utilities/src/lib/collection-utilities.ts
+++ packages/shared-utilities/src/lib/collection-utilities.ts
@@ -23,6 +23,21 @@
     return value !== null && value !== undefined;
   };
   
+  /**
+ * Asserts in a type safe manner that a value is empty (null or undefined).
+ * It can be used in a list::filter to strip defined and non-null values
+ * while informing the next method in the chain that all values
+ * have been sanitized.
+ *
+ * ```
+ * const values = ['a', 'b', null, 'c']
+ * values
+ *   .filter(isEmpty)
+ *   .map(value => value) // all undefined
+ * ```
+ * @param value
+ * @returns
+ */
   export const isEmpty = <TValue>(
     value: TValue | null | undefined,
   ): value is TValue => {
